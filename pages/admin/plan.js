5d3d4bf9009db157666ecc1fec5bdf1a60fa2d7b
hoshi-hiyouga <hiyouga@buaa.edu.cn>
2018-10-23 14:30:01 +0800

    add_schedule
    ~~~

--- pages/admin/plan.js
+++ pages/admin/plan.js
@@ -5,6 +5,7 @@ const app = getApp()
 Page({
 
   data: {
+    showTopTips: false,
     userInfo: {},
     date: "----",
     start_time: "12:00",
@@ -13,33 +14,102 @@ Page({
     typeIndex: 0
   },
 
-  onLoad: function () {
+  onLoad: function() {
     this.setData({
       userInfo: app.globalData.userInfo
     })
   },
 
-  bindDateChange: function (e) {
+  showTopTips: function () {
+    this.setData({
+      showTopTips: true
+    })
+    setTimeout(_ => {
+      this.setData({
+        showTopTips: false
+      })
+    }, 2000)
+  },
+
+  bindDateChange: function(e) {
     this.setData({
       date: e.detail.value
     })
   },
 
-  bindStartTimeChange: function (e) {
+  bindStartTimeChange: function(e) {
     this.setData({
       start_time: e.detail.value
     })
   },
 
-  bindEndTimeChange: function (e) {
+  bindEndTimeChange: function(e) {
     this.setData({
       end_time: e.detail.value
     })
   },
 
-  bindTypeChange: function (e) {
+  bindTypeChange: function(e) {
     this.setData({
       typeIndex: e.detail.value
     })
+  },
+
+  formSubmit: function(e) {
+    var formdata = e.detail.value
+    if (!formdata.name || !formdata.location || !formdata.max_member) {
+      this.showTopTips()
+    } else {
+      var duration = new Date(Date.parse('2000-01-01 ' + formdata.end_time) - Date.parse('2000-01-01 ' + formdata.start_time) + Date.parse('2000-01-01 00:00:00'))
+      wx.request({
+        url: 'https://buaa.hiyouga.top/schedule.php',
+        data: {
+          uid: app.globalData.userId,
+          type: 'add',
+          name: formdata.name,
+          ordertype: this.data.types[formdata.type],
+          date: formdata.date,
+          start_time: formdata.start_time + ':00',
+          duration: duration.toTimeString().substring(0, 8),
+          location: formdata.location,
+          max_member: formdata.max_member,
+          sign: app.makeSign(app.globalData.unique_key)
+          //sign: app.makeSign(app.globalData.unique_key + String(Date.parse(new Date()) / 1000))
+        },
+        method: 'GET',
+        dataType: 'json',
+        success: res => {
+          if (res.data.status == 'success') {
+            wx.showToast({
+              title: '添加成功',
+              icon: 'success',
+              duration: 1650,
+              mask: true,
+              success: res => {
+                setTimeout(function() {
+                  wx.navigateBack({
+                    delta: 1
+                  })
+                }, 2000)
+              }
+            })
+          } else if (res.data.status == 'denied') {
+            wx.showToast({
+              title: '页面过期，请刷新',
+              icon: 'none',
+              duration: 2000,
+              mask: true
+            })
+          } else {
+            wx.showToast({
+              title: '添加失败！_(:з」∠)_',
+              icon: 'none',
+              duration: 2000,
+              mask: true
+            })
+          }
+        }
+      })
+    }
   }
 })
---
+++ server/schedule.php
@@ -0,0 +1,36 @@
+<?php
+error_reporting(0);
+header('Content-type: application/json');
+require_once 'database.php';
+require_once 'util.php';
+$data = array();
+//if (strcmp(md5(getKey($link, $_GET['uid']) . time()), $_GET['sign'])) {
+if (strcmp(md5(getKey($link, $_GET['uid'])), $_GET['sign'])) {
+	$data['status'] = 'denied';
+} else {
+	if ($_GET['type'] == 'add') {
+		$sql = "INSERT INTO schedule (name, type, date, start_at, duration, location, max_member) VALUES ('".$_GET['name']."', '".$_GET['ordertype']."', '".$_GET['date']."', '".$_GET['start_time']."', '".$_GET['duration']."', '".$_GET['location']."', '".$_GET['max_member']."')";
+		mysqli_query($link, $sql);
+		$data = array('status' => 'success');
+	} elseif ($_GET['type'] == 'problem') {
+		$sql = "UPDATE orderform SET has_problem = 1, problem = '"
+		. $_GET['problem_text'] . "' WHERE mid = " . $_GET['mid'];
+		mysqli_query($link, $sql);
+		$data = array('status' => 'success');
+	}
+}
+mysqli_close($link);
+echo json_encode($data);
+exit;
+
+function isfree ($link, $sid) {
+	$sql = "SELECT now_member, max_member FROM schedule WHERE sid = " . $sid;
+	$res = mysqli_query($link, $sql);
+	$row = mysqli_fetch_assoc($res);
+	mysqli_free_result($res);
+	if ($row['now_member'] < $row['max_member']) {
+		return true;
+	} else {
+		return false;
+	}
+}
